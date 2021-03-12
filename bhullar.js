3147466c4500d0f977d53749c3048a7aad18c9c1
rajveerbhullar <rajveerbhullar570@gmail.com>
2021-03-11 21:07:39 -0500

    initial

--- bhullar.js
+++ bhullar.js
@@ -1,3 +1,13 @@
-$('.cinema-seats .seat').on('click', function() {
-    $(this).toggleClass('active');
-  });
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+function SelectedMovieData(movieIndex, moviePrice) {
+	localStorage.setItem("selectedMovieIndex", movieIndex);
+	localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
